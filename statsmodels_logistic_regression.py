73f2287cd2d66827eb21b1533ea758c0f6b24660
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2019-05-20 16:47:41 -0400

    Added errorbars and fixed bugs
    Added error bars to the bar graphs to represent confidence intervals wrt the odds ratios. Also fixed bugs regarding the scale of the odds ratio graph being linear instead of logarithmic.

--- statsmodels_logistic_regression.py
+++ statsmodels_logistic_regression.py
@@ -7,7 +7,7 @@ Created on Mon May  6 12:42:39 2019
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import statsmodels.api as sm
+import statsmodels.formula.api as sm
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -17,6 +17,9 @@ import seaborn as sns
 np.random.seed(42)
 random.seed(42)
 data = pd.read_csv('pancreatic_cancer_smokers.csv')
+features = list(data.columns)
+features.remove('case (1: case, 0: control)')
+features.reverse()
 x=data.values
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(x)
@@ -31,15 +34,40 @@ result = model.fit()
 params = result.params
 conf = result.conf_int()
 conf['OR'] = params
-conf.columns = ['2.5%', '97.5%', 'OR']
+conf.columns = ['2.5%', "97.5%", "OR"]
 
+
+conf = np.exp(conf)
+
+odds_ratios_95 = conf['OR']
+odds_ratios_low = conf['2.5%']
+odds_ratios_high = conf['97.5%']
+low_diff = []
+high_diff = []
+
+for i in range(len(odds_ratios_low)):
+    low_diff.append(odds_ratios_95[i]-odds_ratios_low[i])
+    high_diff.append(odds_ratios_high[i]-odds_ratios_95[i])
+
+asymmetric_error = [low_diff, high_diff]
+
+    
 def plot_hist():
     plt.rcParams['figure.figsize'] = (8.0, 10.0)   
-    params.plot(kind='barh')
+    conf['OR'].plot(kind='barh')
     plt.title('Odds Ratios for Pancreatic Cancer Data')
-    plt.ylabel('Odds Ratios')
-    plt.xlabel('Features')
+    plt.errorbar(y=features, x=odds_ratios_95, xerr = asymmetric_error, ls = 'none')
+    plt.xlabel('Odds Ratios')
+    plt.ylabel('Features')
     plt.savefig('odds_ratios.pdf')
     plt.show()
-    
+   
+def violin_plot():
+    plot = sns.violinplot(data=params)
+    plot.set_xticklabels(plot.get_xticklabels(), rotation=40, ha="right")
+    plot.set(title='Odds Ratios', xlabels='features', ylabel='Odds Ratios')
+    plt.tight_layout
+    plt.savefig('violinplot_OR.pdf')
+    plt.show()
+
 plot_hist()
